6777806425cc308b942c7d23faa8c86a121e4253
Daniel de Sales da Silva <daniel.sales@techne.com.br>
2018-01-19 17:08:20 -0200

    get de users

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -19,6 +19,7 @@ export class UserComponent implements OnInit {
   hobby: string;
   hobbies: string[];
   users: User[];
+  userActive: User;
 
   constructor(private usersService: UsersService) { }
 
